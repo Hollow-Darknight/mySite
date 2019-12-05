220ad5468cd9266c25e8132e60843c8757f4e7cc
Vincent Grondin <vincent.grondin@live.fr>
2019-12-05 17:53:31 +0100

    Mises à jour du 05/12/2019

--- characters/views.py
+++ characters/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404, reverse
 from django.http import HttpResponseRedirect
 
 from myBlog import navigation
-from characters.models import Personnage
+from characters.models import Personnage, Commentaire
+from characters.forms import CreateCommentForm
 
 
 # Create your views here.
@@ -17,13 +18,30 @@ def all_characters(request):
     return render(request, 'characters/view_all.html', context)
 
 
-def character_details(request, slug):
+def character_details(request, slug, message=""):
     prenom = slug.capitalize()
-    personnage = Personnage.objects.get(prenom=prenom)
+    personnage = get_object_or_404(Personnage, prenom=prenom)
+
+    comments = personnage.commentaires.exclude(status=Commentaire.STATUS_HIDDEN).order_by('created_at')
+
+    if request.method == 'POST':
+        comment_form = CreateCommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.personnage = personnage
+            comment.save()
+
+            args = [personnage.slug, 'Votre commentaire a été envoyé !']
+            return HttpResponseRedirect(reverse('character-details-message', args=args) + '#commentaires')
+    else:
+        comment_form = CreateCommentForm()
 
     context = {
         'navigation_items': navigation.navigation_items(navigation.NAV_CHARACTERS),
-        'personnage': personnage
+        'personnage': personnage,
+        'comments': comments,
+        'comment_form': comment_form,
+        'message': message
     }
 
     return render(request, 'characters/character_details.html', context)
