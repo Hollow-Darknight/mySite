0cf3665e9fd0bb9fbc57dabaa48ecbc2376bcdae
Vincent Grondin <vincent.grondin@live.fr>
2019-12-10 17:38:16 +0100

    Petite update du 10/12/2019

--- myBlog/urls.py
+++ myBlog/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('adminPanel/', RedirectView.as_view(url=reverse_lazy('admin:index')), name='adminPanel'),
     path('home/', include('home.urls')),
     path('characters/', include('characters.urls')),
     path('account/', include('accounts.urls')),
